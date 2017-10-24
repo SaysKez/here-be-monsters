a741ebbfa6095870a416699645eed1bad443874e
Kezie Todd <kezle@Ks-MacBook-Pro.local>
2017-10-24 15:42:16 +0100

    added jQuery

--- animation.js
+++ animation.js
@@ -1,2 +1,23 @@
 var tl = new TimelineMax();
 tl.from('#croc', 1, {y:40, opacity: 0.5});
+
+/*
+var green = document.getElementById("swamp-icon");
+var croc = document.getElementById("croc");
+
+
+green.onmouseenter = function(){
+    TweenMax.to(croc, 1, {y:40, opacity: 0.5});
+}
+
+green.onmouseleave = function(){
+    TweenMax.from(croc, 1, {y:40, opacity: 0.5});
+}
+*/
+
+
+$("#swamp-icon").hover(function(){
+    tl.reverse();
+  },function(){
+    tl.play();
+  })
