923714a3a9526f224f414372a594244a555793ec
Kezie Todd <kezle@Ks-MacBook-Pro.local>
2017-10-30 10:51:31 +0000

    added flag hover

--- animation.js
+++ animation.js
@@ -33,9 +33,41 @@ $("#swamp-icon").hover(function(){
   })
 
 
+//KET LOCATION REPEAT
+var shipRepeat = new TimelineMax({repeat:-1, yoyo:true, repeatDelay:0.05});
+shipRepeat.add('start', 0)
+.fromTo('#ship', 1, { rotation:-3, transformOrigin: "50% 50%"}, {rotation:3, transformOrigin: "50% 50%"}, 'start', );
 
+/*
+var flagRepeat = new TimelineMax({repeat:-1});
+flagRepeat.add('start', 0)
+.fromTo();
+*/
 
 //KEY LOCATION HOVERS
+  var shipHover = new TimelineMax({paused: true});
+  shipHover.add('start', 0)
+  shipHover.to('.ship-highlight', 0.5, {fill:"#6ad4eb", stroke:"#6ad4eb"}, 'start')
+  .to('#about-text', 0.5, {text:"Come Sailing", fill: "#6ad4eb"}, 'start');
+
+  $("#about-box").hover(function(){
+    shipHover.play();
+  },function(){
+    shipHover.reverse();
+  })
+
+
+
+  var flagHover = new TimelineMax({paused: true});
+  flagHover.add('start', 0)
+  flagHover.to('.flag-highlight', 0.5, {fill:"#6ad4eb", stroke:"#6ad4eb"}, 'start')
+  .to('#cv-text', 0.5, {text:"Pirate for Hire", fill: "#6ad4eb"}, 'start');
+
+  $("#cv-box").hover(function(){
+    flagHover.play();
+  },function(){
+    flagHover.reverse();
+  })
 
   var krakenhover = new TimelineMax({paused: true});
   krakenhover.add('start', 0)
@@ -47,20 +79,3 @@ $("#swamp-icon").hover(function(){
   },function(){
     krakenhover.reverse();
   })
-
-
-  var shiphover = new TimelineMax({paused: true});
-  shiphover.add('start', 0)
-  shiphover.to('.ship-highlight', 0.5, {fill:"#6ad4eb", stroke:"#6ad4eb"}, 'start')
-  .to('#about-text', 0.5, {text:"Come Sailing", fill: "#6ad4eb"}, 'start');
-
-  $("#about-box").hover(function(){
-    shiphover.play();
-  },function(){
-    shiphover.reverse();
-  })
-
-
-  var keyLocations = new TimelineMax({repeat:-1, yoyo:true, repeatDelay: 0.25});
-  keyLocations.add('start', 0)
-  .fromTo('#ship', 1, {ease: "Back.easeOut", rotation:-5, transformOrigin: "center center"}, {rotation:5, transformOrigin: "center center"}, 'start', );
